51903709c6830d0e8e397d74c89eac323ce716f0
Matthew Nakafuji <mattnakafuji@csu.fullerton.edu>
2023-10-30 19:02:35 -0700

    Added header, added for loop to calculate sum and find average of values given by user.

--- part-2/average.cc
+++ part-2/average.cc
@@ -1,4 +1,7 @@
-// TODO: Add the required header
+// Matthew Nakafuji
+// mattnakafuji@csu.fullerton.edu
+// @MattSequoia
+// Partners: @anupgu16
 
 #include <iostream>
 #include <string>
@@ -7,10 +10,14 @@
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
 
-  // TODO: Validate that there is at least one command line argument.
+  // Validate that there is at least one command line argument.
   // If not, print an error message and return a non-zero value.
+  if (arguments.size() < 2) {
+    std::cout << "error: you must supply at least one argument";
+    return 1;
+  }
 
-  // TODO: Write a for-each loop to sum (add up) all of the command line
+  // Write a for-each loop to sum (add up) all of the command line
   // arguments.
   // Use a double or float type so that your program preserves fractional
   // values.
@@ -18,14 +25,30 @@ int main(int argc, char* argv[]) {
   // of the arguments vector.
   // Each argument is a std::string. You will need to convert each string into
   // a number with the std::stod or std::stof function.
+  double sum{0.0};
+  double value{0.0};
+  bool first{true};
+  for (const std::string& argument : arguments) {
+    if (first) {
+      first = false;
+      continue;
+    }
+    value = std::stod(argument);
+    sum = sum + value;
+  }
 
-  // TODO: After the loop has finished summing the arguments, calculate the
+  // After the loop has finished summing the arguments, calculate the
   // average of the values. Recall that the average is the total value divided
   // by the number of values.
+  double average{0.0};
+  double length{static_cast<double>(arguments.size())};
+  average = sum / (length - 1);
 
-  // TODO: Use cout to print out a message of the form
+  // Use cout to print out a message of the form
   // average = *AVERAGE*
   // on its own line.
+
+  std::cout << "average = " << average << "\n";
 
   return 0;
 }
